cf5e6044803031268032bffc7da41c6f74b3ce22
Sharath Rajasekar <sharathr@gmail.com>
2023-06-20 20:25:04 -0700

    Moved the goroutine into Extract, keeping Notify pretty simple

--- pkg/extractors/intent_extractor.go
+++ pkg/extractors/intent_extractor.go
@@ -2,7 +2,6 @@ package extractors
 
 import (
 	"context"
-	"fmt"
 	"strings"
 	"sync"
 
@@ -29,56 +28,69 @@ func (ee *IntentExtractor) Extract(
 	sessionMutex.Lock()
 	defer sessionMutex.Unlock()
 
-	// Sanity checker: Check if there's exactly one message in messageEvent
-	if len(messageEvent.Messages) != 1 {
-		return NewExtractorError(
-			fmt.Sprintf("IntentExtractor: expected 1 message, got %d", len(messageEvent.Messages)),
-			nil,
-		)
+	errs := make(chan error, len(messageEvent.Messages))
+	var wg sync.WaitGroup
+
+	for _, message := range messageEvent.Messages {
+		wg.Add(1)
+		go func(message models.Message) {
+			defer wg.Done()
+
+			// Populate the template with the message
+			data := IntentPromptTemplateData{
+				Input: message.Content,
+			}
+
+			// Create a prompt with the Message input that needs to be classified
+			prompt, err := internal.ParsePrompt(intentPromptTemplate, data)
+			if err != nil {
+				errs <- NewExtractorError("IntentExtractor: "+err.Error(), err)
+				return
+			}
+
+			// Send the populated prompt to the language model
+			resp, err := llms.RunChatCompletion(ctx, appState, intentMaxTokens, prompt)
+			if err != nil {
+				errs <- NewExtractorError("IntentExtractor: "+err.Error(), err)
+				return
+			}
+
+			// Get the intent from the response
+			intentContent := resp.Choices[0].Message.Content
+			intentContent = strings.TrimPrefix(intentContent, "Intent: ")
+
+			// Put the intent into the message metadata
+			intentResponse := []models.MessageMetadata{
+				{
+					UUID: message.UUID,
+					Metadata: map[string]interface{}{
+						"system": map[string]interface{}{"intent": intentContent},
+					},
+				},
+			}
+
+			// Put the intent into the message metadata
+			log.Debugf("IntentExtractor: intentResponse: %+v", intentResponse)
+			err = appState.MemoryStore.PutMessageMetadata(ctx, appState, sessionID, intentResponse, true)
+			if err != nil {
+				errs <- NewExtractorError(
+					"IntentExtractor failed to put message metadata: "+err.Error(),
+					err,
+				)
+				return
+			}
+		}(message)
 	}
 
-	// As we only have one message in messageEvent, we can directly use it. The iterator is in Notify.
-	message := messageEvent.Messages[0]
+	// Wait for all goroutines to finish
+	wg.Wait()
+	close(errs)
 
-	// Populate the template with the message
-	data := IntentPromptTemplateData{
-		Input: message.Content,
-	}
-
-	// Create a prompt with the Message input that needs to be classified
-	prompt, err := internal.ParsePrompt(intentPromptTemplate, data)
-	if err != nil {
-		return NewExtractorError("IntentExtractor: "+err.Error(), err)
-	}
-
-	// Send the populated prompt to the language model
-	resp, err := llms.RunChatCompletion(ctx, appState, intentMaxTokens, prompt)
-	if err != nil {
-		return NewExtractorError("IntentExtractor: "+err.Error(), err)
-	}
-
-	// Get the intent from the response
-	intentContent := resp.Choices[0].Message.Content
-	intentContent = strings.TrimPrefix(intentContent, "Intent: ")
-
-	// Put the intent into the message metadata
-	intentResponse := []models.MessageMetadata{
-		{
-			UUID: message.UUID,
-			Metadata: map[string]interface{}{
-				"system": map[string]interface{}{"intent": intentContent},
-			},
-		},
-	}
-
-	// Put the intent into the message metadata
-	log.Debugf("IntentExtractor: intentResponse: %+v", intentResponse)
-	err = appState.MemoryStore.PutMessageMetadata(ctx, appState, sessionID, intentResponse, true)
-	if err != nil {
-		return NewExtractorError(
-			"IntentExtractor failed to put message metadata: "+err.Error(),
-			err,
-		)
+	// Check if we got any errors
+	for err := range errs {
+		if err != nil {
+			return NewExtractorError("IntentExtractor: Extract Failed", err)
+		}
 	}
 
 	return nil
@@ -93,42 +105,9 @@ func (ee *IntentExtractor) Notify(
 		return NewExtractorError("IntentExtractor: messageEvent is nil at Notify", nil)
 	}
 
-	var wg sync.WaitGroup
-	errs := make(chan error, len(messageEvent.Messages))
-
-	// Speed up the intent extraction by calling go routines for each message
-	// useful for batch message intent extraction
-	for _, message := range messageEvent.Messages {
-		// Create a new copy of message to avoid data race
-		message := message
-
-		// Create a new single message event
-		singleMessageEvent := &models.MessageEvent{
-			SessionID: messageEvent.SessionID,
-			Messages:  []models.Message{message},
-		}
-
-		// Increment the WaitGroup counter
-		wg.Add(1)
-
-		// Extract the intent in a goroutine
-		go func() {
-			defer wg.Done()
-			if err := ee.Extract(ctx, appState, singleMessageEvent); err != nil {
-				errs <- err
-			}
-		}()
-	}
-
-	// Wait for all goroutines to finish
-	wg.Wait()
-	close(errs)
-
-	// Check if we got any errors
-	for err := range errs {
-		if err != nil {
-			return NewExtractorError("IntentExtractor: Extract Failed", err)
-		}
+	// Call Extract with the entire message event
+	if err := ee.Extract(ctx, appState, messageEvent); err != nil {
+		return NewExtractorError("IntentExtractor: Extract Failed", err)
 	}
 
 	return nil
