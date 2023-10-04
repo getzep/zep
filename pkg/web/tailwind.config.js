601ca85d8c5ada8a20ca548d85f24d796ca9ae3b
Daniel Chalef <131175+danielchalef@users.noreply.github.com>
2023-10-03 18:16:34 -0700

    dark mode

--- pkg/web/tailwind.config.js
+++ pkg/web/tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./templates/**/*.{html,js}", "./static/**/*.{html,js}", "./static/preline/preline.js"],
-  theme: {
-    extend: {},
-  },
   plugins: [
     require('./static/preline/plugin'),
   ],
