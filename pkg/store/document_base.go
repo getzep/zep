f961fc16aba970e5623a0df21b84b8d843a44e7d
Travis Beauvais <travis@getzep.com>
2024-09-24 12:45:34 -0700

    delete all old files

--- pkg/store/document_base.go
+++
@@ -1,7 +0,0 @@
-package store
-
-// BaseDocumentStore is the base implementation of a DocumentStore. Client is the underlying datastore client,
-// such as a database connection.
-type BaseDocumentStore[T any] struct {
-	Client T
-}
--- pkg/web/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class',
-  content: ["./templates/**/*.{html,js}", "./static/**/*.{html,js}", "./static/preline/preline.js"],
-  plugins: [
-    require('./static/preline/plugin'),
-  ],
-}
